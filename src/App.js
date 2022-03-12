44d6f41d12e6c92309b4fa0685da44fd6e6854e3
Jorge Castillo <48936633+ainigiri@users.noreply.github.com>
2022-03-12 01:53:38 -0700

    Revert "question2"

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ import { Container, Row, Col } from 'react-bootstrap';
 import NavigationBar from './components/NavigationBar/NavigationBar'
 
 import Home from './pages/Home/Home'
-import Universities from './pages/Universities/Universities';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <NavigationBar></NavigationBar>
-        <Container className="mt-3" fluid>
+        <Container fluid>
           <Row>
             <Col>
               <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/universities" element={<Universities />} />
-                <Route path="/postal" />
+                <Route path="/" element={<Home />}>
+                  <Route path="universities" />
+                  <Route path="postal" />
+                </Route>
               </Routes>
-              
             </Col>
           </Row>
         </Container>
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,13 +1,9 @@
 import { combineReducers } from "redux";
-import countriesReducer from "./countries/countries.reducer";
 import postsReducer from "./posts/posts.reducer";
-import universitiesReducer from "./universities/universities.reducer";
 
 const rootReducer = () => {
     return combineReducers({
         posts: postsReducer,
-        countries: countriesReducer,
-        universities: universitiesReducer
     });
 };
 
